12c848edfd3ee89263e94f0d58d69d8b8e446bc3
Jainam_59 <jainam59prajapati@gmail.com>
2026-09-20 18:07:22 +0530

    Delete Brain-Glitch-Complete directory

--- Brain-Glitch-Complete/php/leaderboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-require "db.php";
-$stmt=$pdo->query("SELECT username,score FROM leaderboard ORDER BY score DESC, created_at ASC LIMIT 20");
-echo json_encode($stmt->fetchAll());
-?>
